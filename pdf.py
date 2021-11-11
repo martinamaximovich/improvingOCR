264f40762af34ac3364d154e580c06010d19a04a
Martina Maximovich <79213946+martinamaximovich@users.noreply.github.com>
2021-11-11 11:01:00 -0500

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,18 @@
+from collections import Counter
 import PyPDF2
+import rmgarbage
+from rmgarbage import Rmgarbage
 
-txt = input("Enter the name of the PDF file you want to convert to text.\n")
+
+txt = input("Enter the name of the PDF file you wa
 pdfFile = open(txt, 'rb')
 
 pdfReader = PyPDF2.PdfFileReader(pdfFile)
 
 numberOfPages = pdfReader.numPages
+garbageWords = []
+garbage = Rmgarbage()
+garbage.__init__()
 
 for x in range(numberOfPages):
     print()
@@ -13,6 +20,20 @@ for x in range(numberOfPages):
     print()
     print('Page ' + str(x + 1))
     page = pdfReader.getPage(x)
+    allWords = page.extractText()
+    words = allWords.split()
+
+    for word in words:
+        isGarbage = garbage.is_garbage(word)
+        if isGarbage != False:
+            garbageWords.append(word)
+
+    print()
     print(page.extractText())
 
+frequency = Counter(garbageWords)
+print()
+print(frequency)
+
 pdfFile.close()
+~                                                 
