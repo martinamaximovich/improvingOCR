5c25f33a718a7dc5b663d2a2b03aba9faed0152b
martinamaximovich <martina.maximovich@columbia.edu>
2022-04-20 09:26:41 -0400

    fixed bug

--- pdf.py
+++ pdf.py
@@ -59,48 +59,52 @@ class improvingOCR:
     # CLI
     def cli():
         """Process command line arguments."""
-         parser = argparse.ArgumentParser(description='Process OCR Output')
-         parser.add_argument('ocrFile')
-         args = parser.parse_args()
-         fileName = (filepath).rsplit('/', 1)[-1]
+        parser = argparse.ArgumentParser(description='Process OCR Output')
+        parser.add_argument('ocrFile')
+        args = parser.parse_args()
+        fileName = (filepath).rsplit('/', 1)[-1]
 
-         with open(args.ocrFile) as file:
-             content = file.read()
-             words = content.split()
-             garbagecount = 0
-             wordcount = 0
-             garbageWords = []
-             garbage = Rmgarbage()
-             garbage.__init__()
+        with open(args.ocrFile) as file:
+            content = file.read()
+            words = content.split()
+            garbagecount = 0
+            wordcount = 0
+            garbageWords = []
+            garbage = Rmgarbage()
+            garbage.__init__()
 
-             for word in words:
-                     isGarbage = garbage.is_garbage(word)
-                     wordcount += 1
-                     if isGarbage != False:
-                         garbageWords.append(word)
-                         garbagecount += 1
+            for word in words:
+                isGarbage = garbage.is_garbage(word)
+                wordcount += 1
+                if isGarbage != False:
+                    garbageWords.append(word)
+                    garbagecount += 1
 
-         frequency = Counter(garbageWords)
+            frequency = Counter(garbageWords)
 
-         stringWriter = fileName + '.xlsx'
+            stringWriter = fileName + '.xlsx'
 
-         writer = pd.ExcelWriter(stringWriter, engine='xlsxwriter', engine_kwargs= {'options': {'strings_to_formulas': False, 'strings_to_urls': False}})
-         df = pd.DataFrame.from_records(frequency.most_common(), columns=['page','count'])
-         df.to_excel(writer, sheet_name='Sheet1', index=False)
-         writer.save()
+            writer = pd.ExcelWriter(stringWriter, engine='xlsxwriter', engine_kwargs= {'options': {'strings_to_formulas': False, 'strings_to_urls': False}})
 
-         ratio = 0
+            df = pd.DataFrame.from_records(frequency.most_common(), columns=['page','count'])
 
-         #account for divide by 0 error
-         if wordcount == 0:
-             ratio = 100
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
 
-         else:
-             ratio = 100 - ((garbagecount/wordcount) * 100)
-             ratio = int(ratio)
+            writer.save()
 
-         summary = [[fileName, wordcount, garbagecount, ratio]]
-         summaryTable = pd.DataFrame(summary, columns = ["File Name", "Number of Words", "Number of Garbage Words", "Score"])
-         print(summaryTable)
-         print()
+            ratio = 0
+
+            if wordcount == 0:
+                ratio = 100
+
+            else:
+                ratio = 100 - ((garbagecount/wordcount) * 100)
+                ratio = int(ratio)
+
+            summary = [[fileName, wordcount, garbagecount, ratio]]
+
+            summaryTable = pd.DataFrame(summary, columns = ["File Name", "Number of Words", "Number of Garbage Words", "Score"])
+
+            print(summaryTable)
+            print()
 
